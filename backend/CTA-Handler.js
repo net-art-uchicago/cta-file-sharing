8c1e2b10467e6850983af485affca3f0f96b33d1
Anthony Khaiat <92814726+akhaiat2@users.noreply.github.com>
2022-11-08 16:50:53 -0600

    Got lat on console

--- backend/CTA-Handler.js
+++ backend/CTA-Handler.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const axios = require('axios');
+const { response } = require('express');
 
 router.get('/api/cta', async (req, res) => {
-    const response = await axios.get('https://ctabustracker.com/bustime/api/v3/getvehicles?key=gbNab8LFcBWDx5qmtc8Q8dfjb&rt=172&format=json')
-    res.json(response.data)
+    const potato = await axios.get('https://ctabustracker.com/bustime/api/v3/getvehicles?key=gbNab8LFcBWDx5qmtc8Q8dfjb&rt=172&format=json')
+    res.json(potato.data)
+    //Got first lat of bus on route 172
+    console.log(potato.data['bustime-response']['vehicle'][0]['lat'])
 })
-
+//check database --> fetch from CTA
 module.exports = router
