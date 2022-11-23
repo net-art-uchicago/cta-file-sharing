80260dc7b8685110748c50d52efee027eca38ed2
Benjamin Sim <114605416+bensim0305@users.noreply.github.com>
2022-11-22 20:55:43 -0600

    Update update-bus-stops.js

--- backend/update-bus-stops.js
+++ backend/update-bus-stops.js
@@ -1,39 +1,26 @@
 const fs = require('fs')
+const axios = require('axios')
 const path = require('path')
 
-// this is example data to test the script,
-// u're gonna want to replace this with data
-// from the CTA API
+const main = async () => {
+  const res = await axios('https://data.cityofchicago.org/api/views/qs84-j7wh/rows.json?accessType=DOWNLOAD')
+  //res was returned as a nested array
+  const stops = res.data.data.map((stop) => {
+    //stops gets through nested array to just have an array of arrays, each array has 
+    //2 objects: a coordinate (lat and lon), and bus route numbers associated with
+    //stop location
+    const loc = stop[8].slice(7, -1).split(' ').map((point) => parseFloat(point))
+    //loc just making coordinates a float, not a string
+    const routes = (stop[14] === null) ? [] : stop[14].split(',')
+    //line 14 counts for if there are no routes associated with a stop, makes it 
+    //empty array
+    return { loc, routes }
+  })
 
-//getting a list of routes first, helps us establish what routes exist 
-const lst_routes = async () => {
-    const routes = []
-    const routes_data = await fetch('https://ctabustracker.com/bustime/api/v3/getroutes?key=x7L9d2WQ6qvyvBeSkKtAjDmFH&format=json')
-        .then(res => res.json());
-        for (let i = 0; i < routes_data.length; i++) {
-            lst_routes.push(routes_data['bustime-response']['routes'][i]['rt'])
-        }
-    //not sure if the syntax here to access nested dict is [] or ., ask 
-
-
-// constructing stop coordinates. need help with testing the code to make sure it's right, will ask prof
-const stops_coordinates = async() => {
-    const stops = {}
-    const directions = ['North', 'South', 'East', 'West']
-    for (let route = 0; route < lst_routes.length; route++) {
-        for (let dir = 0; dir < directions.length; dir++) {
-            const coordinate = await fetch ('https://ctabustracker.com/bustime/api/v3/getstops?key=x7L9d2WQ6qvyvBeSkKtAjDmFH&rt=${route}&dir=${dir}format=json')
-            if (coordinate) {
-                stops[route] = (coordinate['lat'], coordinate['lon'])
-            } else {
-                pass
-            }
-        }
-    }
+  const data = JSON.stringify(stops, null, 2)
+  // write the data to the file system, into a file called "bus-data.json"
+  const filepath = path.join(__dirname, '../backend/bus-data.json')
+  fs.writeFileSync(filepath, data)
 }
-// convert the object/array into a JSON string
-const data = JSON.stringify(stops, null, 2)
-// write the data to the file system, into a file called "bus-data.json"
-const filepath = path.join(__dirname, '../backend/bus-data.json')
-fs.writeFileSync(filepath, data)
-}
+
+main()
