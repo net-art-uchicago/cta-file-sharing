4cfd98005bdfa76fbf895a82b2614d233df0342b
Anthony Khaiat <92814726+akhaiat2@users.noreply.github.com>
2022-11-22 19:58:17 -0800

    Finished for now

--- backend/CTA-Handler.js
+++ backend/CTA-Handler.js
@@ -28,6 +28,7 @@ function distance(lat1,lat2, lon1, lon2) {
 }
 
 //API/CTA-BUS-DISTANCE
+//Returns JSON of closest distance from closest bus on route to user's location
 router.get('/api/cta-bus-distance', async (req, res) => {
     //From user input (front-end)
     const route = 172
@@ -52,6 +53,18 @@ router.get('/api/cta-bus-distance', async (req, res) => {
         distance: obj_min_distance})
 })
 
+router.get('/api/cta-routes', async (req, res) => {
+    const routes = await axios.get(`https://www.ctabustracker.com/bustime/api/v2/getroutes?key=gbNab8LFcBWDx5qmtc8Q8dfjb&format=json`)
+    lst_route = routes.data['bustime-response']['routes']
+    lst_routes = []
+    for (let i = 0; i < lst_route.length; i++) {
+        lst_routes.push(routes.data['bustime-response']['routes'][i]['rt'])
+    }    
+    res.json({
+        lst_routes})
+})
+
+//Returns JSON of distance to closest stop
 router.get('/api/cta-near-stop', async (req, res) => {
     //Change route to user's input
     const route = 172
@@ -63,28 +76,33 @@ router.get('/api/cta-near-stop', async (req, res) => {
     }
     lst_stops = []
     const first_way_stops = await axios.get(`http://ctabustracker.com/bustime/api/v2/getstops?key=gbNab8LFcBWDx5qmtc8Q8dfjb&rt=${route}&dir=${lst_directions[0]}&format=json`)
-    for (let j = 0; j < first_way_stops['bustime-response']['stops'].length; j++) {
+    for (let j = 0; j < lst_direction.length; j++) {
         lst_stops_temp = []
-        lst_stops_temp.push(first_way_stops.data['bustime-response']['stops'][j][lat])
-        lst_stops_temp
+        lst_stops_temp.push(first_way_stops.data['bustime-response']['stops'][j]['stpnm'])
+        lst_stops_temp.push(first_way_stops.data['bustime-response']['stops'][j]['lat'])
+        lst_stops_temp.push(first_way_stops.data['bustime-response']['stops'][j]['lon'])
+        lst_stops.push(lst_stops_temp)
     }
     if (lst_direction.length == 2) {
         const second_way_stops = await axios.get(`http://ctabustracker.com/bustime/api/v2/getstops?key=gbNab8LFcBWDx5qmtc8Q8dfjb&rt=${route}&dir=${lst_directions[1]}&format=json`)
+        for (let j = 0; j < lst_direction.length; j++) {
+            lst_stops_temp = []
+            lst_stops_temp.push(second_way_stops.data['bustime-response']['stops'][j]['stpnm'])
+            lst_stops_temp.push(second_way_stops.data['bustime-response']['stops'][j]['lat'])
+            lst_stops_temp.push(second_way_stops.data['bustime-response']['stops'][j]['lon'])
+            lst_stops.push(lst_stops_temp)
+        }
     }
-    res.json(first_way_stops.data)
     user_lat = 41.78650608062744
     user_lon = -87.59143829345703
     lst_distance = []
-    //for (let i = 0; i < lst_route_lat_lon.length; i++) {
-    //    lst_distance.push(distance(lst_route_lat_lon[i][0],user_lat, lst_route_lat_lon[i][1], user_lon))
-    //}
-    //obj_min_distance = Math.min(...lst_distance)
-    //res.json(routes.data)
-    //res.json({
-    //    distance: obj_min_distance})
+    for (let i = 0; i < lst_stops.length; i++) {
+        lst_distance.push(distance(lst_stops[i][1],user_lat, lst_stops[i][2], user_lon))
+    }
+    obj_min_distance = Math.min(...lst_distance)
+    res.json({
+        distance: obj_min_distance})
 })
-
-
 
 //Check database --> fetch from CTA
 module.exports = router
