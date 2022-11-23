a8df8f66ccafb410a1130a8975cfc4fcef90a512
bsim0305 <bensim0305@uchicago.edu>
2022-11-23 14:00:07 -0600

    changed rest api to normal

--- backend/REST-API.js
+++ backend/REST-API.js
@@ -1,11 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const axios = require('axios')
 
-router.get('/api/cta', async (req, res) => {
-  const response = await axios.get('ctaHandler')
-  const data = await response.json()
-  res.json(response.data)
+router.get('/api/example', (req, res) => {
+  res.json({ status: 'success', message: 'this is an example' })
 })
 
 module.exports = router
