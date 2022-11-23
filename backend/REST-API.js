07e295196b6f1784f4860b24088e5cecefb7413d
Shayna Kaushal <shaynakaushal@gmail.com>
2022-11-22 22:34:07 -0600

    style seems good?

--- backend/REST-API.js
+++ backend/REST-API.js
@@ -8,7 +8,7 @@ router.use(bodyParser.json())
 
 router.post('/api/add-poem', (req, res) => {
   // TODO: can let this be a JSON object as required
-  addPoem(JSON.stringify(req.body))
+  // addPoem(JSON.stringify(req.body))
 
   // addPoem does not return anything hence post request will return success each time
   res.json(
