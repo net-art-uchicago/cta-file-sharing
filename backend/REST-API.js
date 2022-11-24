6a34073c8ea2df42794b1e362329a06c9d519e52
Shayna Kaushal <shaynakaushal@gmail.com>
2022-11-23 20:39:53 -0600

    style

--- backend/REST-API.js
+++ backend/REST-API.js
@@ -1,31 +1,17 @@
 const express = require('express')
 const router = express.Router()
 const bodyParser = require('body-parser')
-
-// const fs = require('fs')
 const addPoem = require('./rest-api-test-new-data.js')
 
 router.use(bodyParser.json())
 
 router.post('/api/add-poem', (req, res) => {
-  // TODO: can let this be a JSON object as required
-  // addPoem(JSON.stringify(req.body))
-
   const status = addPoem(req.body)
 
   if (status) {
-    res.json(
-      {
-        message: 'success'
-      }
-    )
+    res.json({ message: 'success' })
   } else {
-    res.json(
-      {
-        message: 'failure',
-        error: status
-      }
-    )
+    res.json({ message: 'failure', error: status })
   }
 })
 
