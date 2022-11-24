9b67bf03a00135a852510cefc488cf14bbdffaa3
Anthony Khaiat <92814726+akhaiat2@users.noreply.github.com>
2022-11-23 21:29:36 -0800

    Feature branch

--- backend/CTA-Handler.js
+++ backend/CTA-Handler.js
@@ -10,21 +10,17 @@ function distance(lat1,lat2, lon1, lon2) {
     lon1 =  lon1 * Math.PI / 180
     lon2 = lon2 * Math.PI / 180
     lat1 = lat1 * Math.PI / 180
-    lat2 = lat2 * Math.PI / 180
-
+    lat2 = lat2 * Math.PI / 180 
     // Haversine formula
     let dlon = lon2 - lon1
     let dlat = lat2 - lat1
-    let a = Math.pow(Math.sin(dlat / 2), 2) + Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(dlon / 2),2)
-
-    let c = 2 * Math.asin(Math.sqrt(a))
-
+    let a = Math.pow(Math.sin(dlat / 2), 2) + Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(dlon / 2),2)  
+    let c = 2 * Math.asin(Math.sqrt(a)) 
     // Radius of earth in kilometers. Use 3956
     // for miles
-    let r = 6371
-
+    let r = 6371    
     // calculate the result
-    return(c * r)
+    return (c * r)
 }
 
 //API/CTA-BUS-DISTANCE
@@ -33,7 +29,6 @@ router.get('/api/cta-bus-distance', async (req, res) => {
     //From user input (front-end)
     const route = 172
     const bus = await axios.get(`https://ctabustracker.com/bustime/api/v3/getvehicles?key=gbNab8LFcBWDx5qmtc8Q8dfjb&rt=${route}&format=json`)
-    //res.json(bus.data)
     lst_bus = bus.data['bustime-response']['vehicle']
     lst_bus_lat_lon = []
     for (let i = 0; i < lst_bus.length; i++) {
