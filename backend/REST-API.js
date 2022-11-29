96a7980810150bf454d9d532c0190fa0c727525a
Ishan Malhotra <ishan710@gmail.com>
2022-11-29 14:44:49 -0600

    linting issues fixed

--- backend/REST-API.js
+++ backend/REST-API.js
@@ -1,94 +1,70 @@
-/* global app */
-
 const express = require('express')
 const router = express.Router()
 const {
-  searchPoemByAuthor,
-  searchPoemByLoc,
-  searchPoemByRoute,
   allPoems
 } = require('./rest-api-test-data.js')
 
-router.get('/api/example', (req, res) => {
-  res.json({ status: 'success', message: 'this is an example' })
-})
-
-
-/* 
-*   Get Request: 
+/*
+*   Get Request:
 *   Endpoint URL: /api/poems
 *   Description: Get all poems
 *   example:  "/api/poems?loc=121.121029,23.1029381208&radius=20&author=nick"
 *   Notes: Here searchPoemByAuthor,searchPoemByLoc,searchPoemByRoute are implemented by the database team
 */
 
-
 router.get('/api/poems', (req, res) => {
   const author = req.query.author
-  let loc = req.query.loc 
+  let loc = req.query.loc
   if (loc) loc = loc.split(',')
   console.log(loc)
   const radius = req.query.radius
   const route = req.query.route
-  const success = false
-  
-  const filter = 
+
+  const filter =
     {
-      "author": author,
-      "lat": loc ? parseInt(loc[0]) : null,
-      "long": loc ? parseInt(loc[1]) : null,
-      "radius": radius ? parseInt(radius) : 20,
-      "route": route
+      author,
+      lat: loc ? parseInt(loc[0]) : null,
+      long: loc ? parseInt(loc[1]) : null,
+      radius: radius ? parseInt(radius) : 20,
+      route
     }
-  
 
-  let msg = ""
-  let poem_list = []
+  let msg = ''
+  let poemList = []
 
-  if (loc && loc.length !==2)
-  {
-      success = false
-      msg = "Invalid Location; expected 2 coordinates, got more"
-      res.send({"poem":null, "status" : true, "msg": msg})
+  if (loc && loc.length !== 2) {
+    msg = 'Invalid Location; expected 2 coordinates, got more'
+    res.send({ poem: null, status: false, msg })
   }
-  {
-    poem_list = allPoems()
-    console.log(filter)
-    poem_list= poem_list.filter(function(item) {
-      for (var key in filter) {
-        if (key == "lat" || key == "long" || key == "radius"){
-          console.log(filter["lat"],item["lat"],filter["long"],item["long"],filter["radius"])
-          console.log("distance" ,getDistance(filter["lat"],filter["long"],item["lat"],item["long"]))
-          if (getDistance(filter["lat"],filter["long"],item["lat"],item["long"]) > filter["radius"]){
-          console.log("exitnt")
+  poemList = allPoems()
+  console.log(filter)
+  poemList = poemList.filter(function (item) {
+    for (const key in filter) {
+      if (key === 'lat' || key === 'long' || key === 'radius') {
+        console.log(filter.lat, item.lat, filter.long, item.long, filter.radius)
+        console.log('distance', getDistance(filter.lat, filter.long, item.lat, item.long))
+        if (getDistance(filter.lat, filter.long, item.lat, item.long) > filter.radius) {
           return false
-          }
         }
-        else if (filter[key] && item[key] != filter[key]){
-          console.log("exitntfvfv")
-          return false;
-        }
+      } else if (filter[key] && item[key] != filter[key]) {
+        return false
       }
-      
-      return true;
-    });
+    }
+    return true
+  })
+  function getDistance (x1, y1, x2, y2) {
+    const y = x2 - x1
+    const x = y2 - y1
+
+    return Math.sqrt(x * x + y * y)
   }
-  
-  function getDistance(x1, y1, x2, y2){
-    let y = x2 - x1;
-    let x = y2 - y1;
-    
-    return Math.sqrt(x * x + y * y);
-}
 
-  res.send({"poem_list": poem_list, 
-            "length": poem_list.length, 
-            "status" : true, 
-            "msg": null})
-
+  res.send({
+    poemList,
+    length: poemList.length,
+    status: true,
+    msg: 'poems sent'
+  })
 })
 
-
-
 module.exports = router
-
--- backend/rest-api-test-data.js
+++ backend/rest-api-test-data.js
@@ -1,51 +1,50 @@
-/* global app */
 const testData = [
-    {
+  {
     poem: 'abc',
     time: 1,
     author: 'nick',
     lat: 3,
     long: 4,
     route: 123
-    },
-    {
+  },
+  {
     poem: 'abc2',
     time: 1,
     author: 'nick',
     lat: 3,
     long: 4,
     route: 123
-    },
-    {
+  },
+  {
     poem: 'abc3',
     time: 2,
     author: 'nick',
     lat: 4,
     long: 4,
     route: 124
-    }, 
-    {
+  },
+  {
     poem: 'abc4',
     time: 1,
     author: 'tina',
     lat: 4,
     long: 4,
     route: 124
-    }, 
-    {
+  },
+  {
     poem: 'abc5',
     time: 1,
     author: 'tina',
     lat: 3,
     long: 5,
     route: 123
-    }
-  ]
-  
-  function allPoems(){
-    return testData
   }
-  
-  module.exports = {
-    allPoems,
-  }
+]
+
+function allPoems () {
+  return testData
+}
+
+module.exports = {
+  allPoems
+}
