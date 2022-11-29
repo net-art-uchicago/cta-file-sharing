8aa9a0ad79f05a63dae825600495c27fe9974936
arera <you@example.com>
2022-11-29 16:52:01 -0600

    connected page to poem database

--- frontend/js/gather.js
+++ frontend/js/gather.js
@@ -2,7 +2,7 @@ const name = document.querySelector('#name')
 const poem = document.querySelector('#poem')
 const time = document.querySelector('#time')
 
-const url = 'package.json'
+const url = 'database.json'
 const req = { method: 'GET' }
 fetch(url, req)
   // turn json information into js object
@@ -11,8 +11,13 @@ fetch(url, req)
   .then(data => {
     const obj = JSON.parse(data)
     // call author of poem... right now it is calling to the arbirtray package it successfully displays
-    name.textContent = obj.name // change name to poem author
-    poem.textContent = obj.description // change description to poem
-    time.textContent = obj.name // change to time
+    name.textContent = obj.author // change name to poem author
+    poem.textContent = obj.poem // change description to poem
+    time.textContent = obj.date // change to time
     console.log(data)
+    console.log(date)
   })
+
+async function getTime () {
+  const res = await fetch('')
+}
