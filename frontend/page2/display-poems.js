40f202480093957e003812ba398458879826e258
aureliejiao <41423520+aureliejiao@users.noreply.github.com>
2022-11-29 14:36:33 -0600

    updating

--- frontend/page2/display-poems.js
+++ frontend/page2/display-poems.js
@@ -1,21 +1,21 @@
 getPoems()
 
 function getPoems () {
-    const req = { method: 'GET'}
-    fetch('poems.json', req)
-        .then(res => res.json())
-        .then(data => appendData(data))
-        .catch(err => console.log('error: ' + err))
-}  
+  const req = { method: 'GET' }
+  fetch('poems.json', req)
+    .then(res => res.json())
+    .then(data => appendData(data))
+    .catch(err => console.log('error: ' + err))
+}
 
 function appendData (data) {
-    const mainCont = document.querySelector("#poem")
+  const mainCont = document.querySelector('#poem')
 
-    for (let i = 0; i < data.length; i++){
-        let div = document.createElement("div");
-        div.innerHTML = `${data[i].text} ...signed ${data[i].author}
-                        // ${data[i].location} // ${data[i].datetime}
-                        // ROUTE ${data[i].route}`;
-        mainCont.appendChild(div)
-    }
+  for (let i = 0; i < data.length; i++) {
+    const div = document.createElement('div')
+    div.innerHTML = `${data[i].text} ...signed ${data[i].author} --
+                        ${data[i].location} -- ${data[i].datetime}  
+                        -- ROUTE ${data[i].route}`
+    mainCont.appendChild(div)
+  }
 }
