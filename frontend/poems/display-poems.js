0c281de7f61da0be38db2baa274afcb6f79922d1
aureliejiao <41423520+aureliejiao@users.noreply.github.com>
2022-12-05 15:01:24 -0600

    poems display working

--- frontend/poems/display-poems.js
+++ frontend/poems/display-poems.js
@@ -19,13 +19,27 @@ function getPoems () {
 }
 
 function displayData (data) {
-  const mainCont = document.querySelector('#poem')
-  let rand = Math.floor(Math.random() * data.length)
-  let text = `<b>${data[rand].poem}</b> 
-  <br><br> ...signed <b>${data[rand].author}</b> 
-  <br><br>(${data[rand].lat}, ${data[rand].long})
-  <br> ✩ date... ${(data[rand].date)}
-  <br> ✩ route... ${data[rand].route} `
+  
+  
+  var i = 0
+  function myLoop(){
+    let rand = Math.floor(Math.random() * data.length)
+    let text = `<b>${data[rand].poem}</b> 
+      <br><br> ...signed <b>${data[rand].author}</b> 
+      <br><br> ✩ (${data[rand].lat}, ${data[rand].long})
+      <br> ✩ date... ${(data[rand].date)}
+      <br> ✩ route... ${data[rand].route}`
+    setTimeout(function() {
+      addBubble(text, "bubble")
+      i++
+      if (i < 3){
+        myLoop()
+      }
+    }, 3000)
+  }
 
-  addBubble(text,'bubble')
+  myLoop()
+
 }
+
+
