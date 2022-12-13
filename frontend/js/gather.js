60ecf8e0954eb38fa0e95da2ae113547c5fce69d
arera <you@example.com>
2022-12-12 19:54:13 -0600

    added logic to display the user's poem

--- frontend/js/gather.js
+++ frontend/js/gather.js
@@ -13,21 +13,41 @@ async function getPoems () {
   const past = Date.now() - min
   leap = data.poemList.filter(p => p.datetime > past)
   // show oldest first to user's poem
+  console.log(leap)
   showPoem()
 }
 // update html page
 function showPoem () {
   // poem is variable as div element, p is the data passing
   const poem = document.querySelector('#poem')
+  const author = document.querySelector('#author')
+  const usage = document.querySelector('.usage')
   const p = leap[i]
   poem.textContent = p.text
+  author.textContent = p.author
+  usage.textContent = 'click anywhere to see the other poems'
   i++
   // how many items in the array??
-  if (i >= leap.length) {
+  if (i > leap.length) {
     // display oldest poem ... later change to user poem
     i = 0
   }
+  else if (i === leap.length) {
+    usage.textContent = p.author
+    console.log(i)
+    i = 0
+  }
+  // what to do on the last click?
+  // else () {
+  // showExit()
+  // }
 }
+// !!! find a way back to the map or writing another poem ,,,
+// especially important if the bus is late and someone wants
+// to write another poem maybe limit user to write a poem every
+// 10 minutes
+// function showExit () {
+// }
 
 // click through animation, on click cycle through pages with poems
 // function for mouseclick
